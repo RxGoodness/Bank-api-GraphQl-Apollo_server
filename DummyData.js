946d20377ed492d786212a3123a54de263edef7c
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-23 22:46:55 +0100

    initial start-up

---
+++ DummyData.js
@@ -0,0 +1,38 @@
+const UserList = [
+    {
+        id: 1,
+        name: 'Adam Scott',
+        username: 'adam',
+        age: 20,
+        nationality:'Canada'
+    },
+    {
+        id: 2,
+        name: 'Pedro Scott',
+        username: 'Pedro',
+        age: 30,
+        nationality:'Brazil'
+    },
+    {
+        id: 3,
+        name: 'Oscar Scott',
+        username: 'oscar',
+        age: 40,
+        nationality:'Germany'
+    },
+    {
+        id: 4,
+        name: 'Mount Scott',
+        username: 'mount',
+        age: 50,
+        nationality:'England'
+    },
+    {
+        id: 5,
+        name: 'Silver Scott',
+        username: 'silver',
+        age: 60,
+        nationality:'nigeria'
+    }
+];
+export default UserList
