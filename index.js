71be6ccd725e417e4d8c8aeae0d24e6a41155791
rxgoodness <adedokuntoyyib@gmail.com>
2022-06-23 21:26:30 +0100

    start

---
+++ index.js
@@ -0,0 +1,7 @@
+import {ApolloServer} from "apollo-server";
+
+const server = new ApolloServer({typeDefs, resolvers});
+server.listen().then(({url}) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
+
